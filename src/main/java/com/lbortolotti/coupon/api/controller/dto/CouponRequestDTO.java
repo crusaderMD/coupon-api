b66fbc1568586bc328f4a5cdab4ba9541aa82d0e
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2026-02-23 20:36:44 -0300

    feat: request and response DTOs

--- src/main/java/com/lbortolotti/coupon/api/controller/dto/CouponRequestDTO.java
+++ src/main/java/com/lbortolotti/coupon/api/controller/dto/CouponRequestDTO.java
@@ -1,4 +1,52 @@
 package com.lbortolotti.coupon.api.controller.dto;
 
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
 public class CouponRequestDTO {
+
+    @NotBlank(message = "Code is required")
+    private String code;
+
+    @NotNull(message = "Discount is required")
+    @Min(value = 1, message = "Discount must be at least 1")
+    @Max(value = 50, message = "Discount must be at least 50")
+    private Integer discount;
+
+    @NotNull(message = "Expiration date is required")
+    @FutureOrPresent(message = "Expiration date must be today or in the future")
+    private LocalDate expirationDate;
+
+    public CouponRequestDTO() {}
+
+    public CouponRequestDTO(String code, Integer discount, LocalDate expirationDate) {
+        this.code = code;
+        this.discount = discount;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
--- src/main/java/com/lbortolotti/coupon/api/controller/dto/CouponResponseDTO.java
+++ src/main/java/com/lbortolotti/coupon/api/controller/dto/CouponResponseDTO.java
@@ -1,4 +1,55 @@
 package com.lbortolotti.coupon.api.controller.dto;
 
+import java.time.LocalDate;
+
 public class CouponResponseDTO {
+
+    private Long id;
+
+    private String code;
+
+    private Integer discount;
+
+    private LocalDate expirationDate;
+
+    public CouponResponseDTO() {}
+
+    public CouponResponseDTO(Long id, String code, Integer discount, LocalDate expirationDate) {
+        this.id = id;
+        this.code = code;
+        this.discount = discount;
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        discount = discount;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
