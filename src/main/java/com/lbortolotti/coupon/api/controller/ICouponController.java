28cd96247303bc7f7ff90f618ff1d88c549fc456
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2026-02-24 20:36:45 -0300

    feat: add swagger anotations to Icontroller

--- src/main/java/com/lbortolotti/coupon/api/controller/ICouponController.java
+++ src/main/java/com/lbortolotti/coupon/api/controller/ICouponController.java
@@ -2,10 +2,24 @@ package com.lbortolotti.coupon.api.controller;
 
 import com.lbortolotti.coupon.api.controller.dto.CouponRequestDTO;
 import com.lbortolotti.coupon.api.controller.dto.CouponResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Coupons", description = "Operations related to coupons")
 public interface ICouponController {
 
+    @Operation(
+            summary = "Create a new coupon",
+            description = "Creates a coupon with code, discount and expiration date"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Coupon successfully created"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "409", description = "Coupon already exists")
+    })
     ResponseEntity<CouponResponseDTO> createCoupon(CouponRequestDTO couponRequestDTO);
 
 
