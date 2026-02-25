6d886c35629faa03ac4b382d01f7ed886f9974d5
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2026-02-25 20:24:46 -0300

    feat. add swagger schema to requestDTO

--- src/main/java/com/lbortolotti/coupon/api/controller/dto/CouponRequestDTO.java
+++ src/main/java/com/lbortolotti/coupon/api/controller/dto/CouponRequestDTO.java
@@ -1,19 +1,29 @@
 package com.lbortolotti.coupon.api.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
+@Schema(name = "CouponRequestDTO",
+        description = "Payload used to create a new coupon with information provided by the client.")
 public class CouponRequestDTO {
 
+    @Schema(description = "Unique identifier of the coupon. Provided by the client. " +
+            "Alphanumeric string, optionally containing hyphens",
+            example = "DESC-50")
     @NotBlank(message = "Code is required")
     private String code;
 
+    @Schema(description = "Discount percentage value between 1 and 50 applied in the coupon. Provided by the client.",
+        example = "10")
     @NotNull(message = "Discount is required")
     @Min(value = 1, message = "Discount must be at least 1")
-    @Max(value = 50, message = "Discount must be at least 50")
+    @Max(value = 50, message = "Discount must not exceed 50")
     private Integer discount;
 
+    @Schema(description = "Date when the coupon expires. Must be today or later. Provided by the client",
+        example = "2026-02-25")
     @NotNull(message = "Expiration date is required")
     @FutureOrPresent(message = "Expiration date must be today or in the future")
     private LocalDate expirationDate;
