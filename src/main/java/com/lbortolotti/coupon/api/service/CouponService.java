e0555dff4546c8a38f104be4e37fd7ccadfa279e
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2026-02-26 19:52:13 -0300

    refac. coupon service

--- src/main/java/com/lbortolotti/coupon/api/service/CouponService.java
+++ src/main/java/com/lbortolotti/coupon/api/service/CouponService.java
@@ -3,6 +3,7 @@ package com.lbortolotti.coupon.api.service;
 import com.lbortolotti.coupon.api.controller.dto.CouponRequestDTO;
 import com.lbortolotti.coupon.api.controller.dto.CouponResponseDTO;
 import com.lbortolotti.coupon.api.domain.Coupon;
+import com.lbortolotti.coupon.api.exception.CouponAlreadyExistsException;
 import com.lbortolotti.coupon.api.mapper.CouponMapper;
 import com.lbortolotti.coupon.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class CouponService implements ICouponService{
     public CouponResponseDTO saveCoupon(CouponRequestDTO requestDTO) {
 
         if (couponRepository.existsByCode(requestDTO.getCode())) {
-            throw new RuntimeException("Coupon already exists");
+            throw new CouponAlreadyExistsException(requestDTO.getCode());
         }
 
         Coupon coupon = CouponMapper.toEntity(requestDTO);
