78bea986ad7bd8991bb36593284357b16a3a7c80
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2026-02-28 17:20:00 -0300

    feat. generic exception handler

--- src/main/java/com/lbortolotti/coupon/api/error/ApiErrorCode.java
+++ src/main/java/com/lbortolotti/coupon/api/error/ApiErrorCode.java
@@ -1,7 +1,9 @@
 package com.lbortolotti.coupon.api.error;
 
 public enum ApiErrorCode {
-    COUPON_ALREADY_EXISTS("Coupon already exists");
+    COUPON_ALREADY_EXISTS("Coupon already exists"),
+    INTERNAL_SERVER_ERROR("Unexpected internal error");
+
     private final String defaultMessage;
 
     ApiErrorCode(String defaultMessage) {
--- src/main/java/com/lbortolotti/coupon/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lbortolotti/coupon/api/exception/GlobalExceptionHandler.java
@@ -10,6 +10,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception ex, HttpServletRequest request) {
+        ErrorResponseDTO response = ErrorResponseDTO.of(HttpStatus.INTERNAL_SERVER_ERROR,
+                ApiErrorCode.INTERNAL_SERVER_ERROR.getDefaultMessage(),
+                ApiErrorCode.INTERNAL_SERVER_ERROR,
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
     @ExceptionHandler(CouponAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDTO> handleCouponAlreadyExists(CouponAlreadyExistsException ex, HttpServletRequest request) {
         ErrorResponseDTO response = ErrorResponseDTO.of(HttpStatus.CONFLICT,
