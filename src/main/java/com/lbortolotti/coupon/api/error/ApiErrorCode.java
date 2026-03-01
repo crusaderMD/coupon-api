fd2283dfd7b17f4a4fffc249f9cbeafdbfcc6bc9
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2026-03-01 12:26:09 -0300

    refact. Api error code to handler Http status

--- src/main/java/com/lbortolotti/coupon/api/error/ApiErrorCode.java
+++ src/main/java/com/lbortolotti/coupon/api/error/ApiErrorCode.java
@@ -1,14 +1,22 @@
 package com.lbortolotti.coupon.api.error;
 
-public enum ApiErrorCode {
-    COUPON_ALREADY_EXISTS("Coupon already exists"),
-    INTERNAL_SERVER_ERROR("Unexpected internal error"),
-    VALIDATION_ERROR("Validation error");
+import org.springframework.http.HttpStatus;
 
+public enum ApiErrorCode {
+    COUPON_ALREADY_EXISTS(HttpStatus.CONFLICT,"Coupon already exists"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected internal error"),
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "Validation error");
+
+    private final HttpStatus httpStatus;
     private final String defaultMessage;
 
-    ApiErrorCode(String defaultMessage) {
+    ApiErrorCode(HttpStatus httpStatus, String defaultMessage) {
+        this.httpStatus = httpStatus;
         this.defaultMessage = defaultMessage;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 
     public String getDefaultMessage() {
